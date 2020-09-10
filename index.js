1df0fa21e8394217fffbb2a6a5f8abfefda405d2
Miley Wright <dishaliekwright@yahoo.com>
2020-09-10 00:34:49 -0400

    js practice

---
+++ index.js
@@ -0,0 +1,46 @@
+//Task a: declare a variable called drinkingAge, console log true if age >= 21 (no function required)
+
+
+ 
+// Task b: create a function that takes a number and returns it's value doubled
+
+
+
+// Task 2: Write a function - given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+function lessThan100(/* code here */){
+    /* Code here */
+}
+
+
+
+// Task 3: A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 21 and older and when he's not on break.
+
+// Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
+
+function shouldServeDrinks(/*code here*/) {
+    // code here
+}
+
+
+
+// Task 4a: Write a function that converts hours into seconds.
+
+
+
+// Task 5: Write a function that finds the sum of the first n natural numbers.
+// for example: 
+//                 sum(5) ➞ 15
+//                 // 1 + 2 + 3 + 4 + 5 = 15
+
+function total(/*code here*/){
+    // code here
+}
+
+
+
+/* uncomment the console.log() to check your work*/
+// console.log(total(5)) // 15
+// console.log(total(10)) // 55
+// console.log(total(12)) // 78
+
